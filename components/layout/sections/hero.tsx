f74f13df88ee728a263adaf60312202f01e10c58
Ando <65276708+andostronaut@users.noreply.github.com>
2024-08-11 13:24:12 +0300

    feat: update ui hero use bg grid and add code block

--- components/layout/sections/hero.tsx
+++ components/layout/sections/hero.tsx
@@ -1,77 +1,77 @@
-"use client";
-import { Badge } from "@/components/ui/badge";
+import { ChevronRight, Github } from "lucide-react";
+
 import { Button } from "@/components/ui/button";
-import { ArrowRight } from "lucide-react";
-import { useTheme } from "next-themes";
-import Image from "next/image";
-import Link from "next/link";
+import GridPattern from "@/components/magicui/grid-pattern";
+
+import { cn } from "@/lib/utils";
 
 export const HeroSection = () => {
-  const { theme } = useTheme();
   return (
-    <section className="container w-full">
-      <div className="grid place-items-center lg:max-w-screen-xl gap-8 mx-auto py-20 md:py-32">
+    <section className="container w-full h-[85vh] relative">
+      <div className="relative z-10 grid place-items-center lg:max-w-screen-xl gap-8 mx-auto py-20 md:py-32">
         <div className="text-center space-y-8">
-          <Badge variant="outline" className="text-sm py-2">
-            <span className="mr-2 text-primary">
-              <Badge>New</Badge>
-            </span>
-            <span> Design is out now! </span>
-          </Badge>
-
           <div className="max-w-screen-md mx-auto text-center text-4xl md:text-6xl font-bold">
             <h1>
-              Experience the
-              <span className="text-transparent px-2 bg-gradient-to-r from-[#D247BF] to-primary bg-clip-text">
-                Shadcn
-              </span>
-              landing page
+              A easy way to implement and write Selenium with TypeScript using
+              Deno
             </h1>
           </div>
 
-          <p className="max-w-screen-sm mx-auto text-xl text-muted-foreground">
-            {`We're more than just a tool, we're a community of passionate
-            creators. Get access to exclusive resources, tutorials, and support.`}
-          </p>
-
           <div className="space-y-4 md:space-y-0 md:space-x-4">
-            <Button className="w-5/6 md:w-1/4 font-bold group/arrow">
-              Get Started
-              <ArrowRight className="size-5 ml-2 group-hover/arrow:translate-x-1 transition-transform" />
-            </Button>
+            <CodeBlock />
 
-            <Button
-              asChild
-              variant="secondary"
-              className="w-5/6 md:w-1/4 font-bold"
-            >
-              <Link
-                href="https://github.com/nobruf/shadcn-landing-page.git"
-                target="_blank"
+            <div className="mt-6 gap-2 flex justify-center">
+              <Button className="w-5/6 md:w-1/4 font-bold group/arrow ">
+                Documentation
+                <ChevronRight className="size-5 ml-2 group-hover/arrow:translate-x-1 transition-transform" />
+              </Button>
+              <Button
+                className="w-5/6 md:w-1/4 font-bold group/arrow"
+                variant="outline"
               >
-                Github respository
-              </Link>
-            </Button>
+                <Github className="size-5 mr-2" />
+                Github
+                <ChevronRight className="size-5 ml-2 group-hover/arrow:translate-x-1 transition-transform" />
+              </Button>
+            </div>
           </div>
         </div>
-
-        <div className="relative group mt-14">
-          <div className="absolute top-2 lg:-top-8 left-1/2 transform -translate-x-1/2 w-[90%] mx-auto h-24 lg:h-80 bg-primary/50 rounded-full blur-3xl"></div>
-          <Image
-            width={1200}
-            height={1200}
-            className="w-full md:w-[1200px] mx-auto rounded-lg relative rouded-lg leading-none flex items-center border border-t-2 border-secondary  border-t-primary/30"
-            src={
-              theme === "light"
-                ? "/hero-image-light.jpeg"
-                : "/hero-image-dark.jpeg"
-            }
-            alt="dashboard"
-          />
-
-          <div className="absolute bottom-0 left-0 w-full h-20 md:h-28 bg-gradient-to-b from-background/0 via-background/50 to-background rounded-lg"></div>
-        </div>
       </div>
+
+      <GridPattern
+        squares={[
+          [4, 4],
+          [5, 1],
+          [8, 2],
+          [5, 3],
+          [5, 5],
+          [10, 10],
+          [12, 15],
+          [15, 10],
+          [10, 15],
+          [15, 10],
+          [10, 15],
+          [15, 10],
+        ]}
+        className={cn(
+          "[mask-image:radial-gradient(600px_circle_at_center,white,transparent)]",
+          "inset-x-0 inset-y-[-30%]"
+        )}
+      />
     </section>
   );
 };
+
+const CodeBlock = () => {
+  const code = "curl -fsSL https://deno.land/install.sh | sh";
+
+  return (
+    <div className="rounded-lg p-4 text-white max-w-xl mx-auto">
+      <div className="p-4 font-mono bg-black rounded-lg mt-4 border border-secondary">
+        <code>{code}</code>
+      </div>
+    </div>
+  );
+};
+
+export default CodeBlock;
