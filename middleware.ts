575e8d43f93dd888f33c6da17970a10175ad0cc9
Ando <65276708+andostronaut@users.noreply.github.com>
2024-08-12 16:35:49 +0300

    feat: specify path matcher on export config in middleware

--- middleware.ts
+++ middleware.ts
@@ -3,3 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(req: NextRequest) {
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
