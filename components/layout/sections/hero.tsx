ce7d08252d2a312b94fdd90510474dfb28282916
Ando <65276708+andostronaut@users.noreply.github.com>
2024-08-26 17:18:18 +0300

    feat: add badge to introduce drowser studio in hero

--- components/layout/sections/hero.tsx
+++ components/layout/sections/hero.tsx
@@ -8,46 +8,52 @@ import { Button } from '@/components/ui/button'
 import AnimatedGridPattern from '@/components/magicui/animated-grid-pattern'
 import { BorderBeam } from '@/components/magicui/border-beam'
 import BlurFade from '@/components/magicui/blur-fade'
+import AnimatedGradientText from '@/components/magicui/animated-gradient-text'
 
 import { cn } from '@/lib/utils'
 
 export const HeroSection = () => {
   return (
-    <section className="w-full relative">
-      <div className="container relative z-10 grid place-items-center lg:max-w-screen-xl gap-8 mx-auto py-20 md:py-28">
-        <div className="space-y-8">
+    <section className='w-full relative'>
+      <div className='container relative z-10 grid place-items-center lg:max-w-screen-xl gap-8 mx-auto py-20 md:py-28'>
+        <div className='space-y-8'>
           <BlurFade delay={0.1}>
-            <div className="mx-auto text-center text-7xl md:text-9xl font-bold">
+            <AnimatedGradientText className='bg-white dark:bg-slate-950'>
+              <span className='backdrop-blur-3xl'>Introducing Drowser Studio</span>
+              <ChevronRight className='ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5' />
+            </AnimatedGradientText>
+          </BlurFade>
+
+          <BlurFade delay={0.1}>
+            <div className='mx-auto text-center text-7xl md:text-9xl font-bold'>
               <h1>Explore new way to write tests</h1>
             </div>
           </BlurFade>
 
-          <div className="space-y-4 md:space-y-0 md:space-x-4">
+          <div className='space-y-4 md:space-y-0 md:space-x-4'>
             <BlurFade delay={0.2}>
               <CodeBlock />
             </BlurFade>
 
             <BlurFade delay={0.3}>
-              <div className="mt-6 gap-2 flex justify-center">
+              <div className='mt-6 gap-2 flex justify-center'>
                 <Button
-                  className="w-5/6 md:w-1/4 font-bold group/arrow"
-                  onClick={() => {
-                    window.open('/docs', '_blank')
-                  }}
+                  className='w-5/6 md:w-1/4 font-bold group/arrow'
+                  onClick={() => window.open('/docs', '_blank')}
                 >
                   Documentation
-                  <ChevronRight className="size-5 ml-2 group-hover/arrow:translate-x-1 transition-transform" />
+                  <ChevronRight className='size-5 ml-2 group-hover/arrow:translate-x-1 transition-transform' />
                 </Button>
                 <Button
-                  className="w-5/6 md:w-1/4 font-bold group/arrow"
-                  variant="outline"
+                  className='w-5/6 md:w-1/4 font-bold group/arrow'
+                  variant='outline'
                   onClick={() => {
                     window.open('https://github.com/kinotio/drowser', '_blank')
                   }}
                 >
-                  <Github className="size-5 mr-2" />
+                  <Github className='size-5 mr-2' />
                   Github
-                  <ChevronRight className="size-5 ml-2 group-hover/arrow:translate-x-1 transition-transform" />
+                  <ChevronRight className='size-5 ml-2 group-hover/arrow:translate-x-1 transition-transform' />
                 </Button>
               </div>
             </BlurFade>
@@ -86,15 +92,11 @@ const CodeBlock = () => {
   `
 
   return (
-    <div className="mx-auto max-w-4xl relative p-4 bg-black rounded-lg mt-4 border border-secondary hidden md:block">
-      <SyntaxHighlighter
-        language="typescript"
-        style={tomorrowNightBright}
-        wrapLongLines
-      >
+    <div className='mx-auto max-w-4xl relative p-4 bg-black rounded-lg mt-4 border border-secondary hidden md:block'>
+      <SyntaxHighlighter language='typescript' style={tomorrowNightBright} wrapLongLines>
         {code}
       </SyntaxHighlighter>
-      <BorderBeam colorFrom="#fff" colorTo="#fff" />
+      <BorderBeam colorFrom='#fff' colorTo='#fff' />
     </div>
   )
 }
